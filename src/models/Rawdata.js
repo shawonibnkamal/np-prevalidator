6867b932e608fd431bdafb3cc349c64091aa1a8b
shawonibnkamal <shawon.1165@gmail.com>
2022-04-01 08:28:03 -0230

    Fix undefined method

--- src/models/Rawdata.js
+++ src/models/Rawdata.js
@@ -1,70 +1,68 @@
 const csvtojson = require("csvtojson");
-const fs=require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 class Rawdata {
-    constructor() {
-        this._filesMap = new Map();
-        this._directory = null;
-        
-    }
+  constructor() {
+    this._filesMap = new Map();
+    this._directory = null;
+  }
 
-    // Set from directory
-    setFromDirectory (directory) {
-        // directory = "/Users/shawonibnkamal/Documents/Honours Project/Sample datasets/Sample dataset 2 birds/RankinData"; 
-        this._directory = directory;
-        this._filesMap = this._getAllFiles(directory);
-    }
+  // Set from directory
+  setFromDirectory(directory) {
+    // directory = "/Users/shawonibnkamal/Documents/Honours Project/Sample datasets/Sample dataset 2 birds/RankinData";
+    this._directory = directory;
+    this._filesMap = this._getAllFiles(directory);
+  }
 
-    // Helper function to get list of all files recursively
-    _getAllFiles (dirPath, filesMap) {
-        let files = fs.readdirSync(dirPath)
+  // Helper function to get list of all files recursively
+  _getAllFiles(dirPath, filesMap) {
+    let files = fs.readdirSync(dirPath);
 
-        filesMap = filesMap || new Map();
+    filesMap = filesMap || new Map();
 
-        files.forEach(function(file) {
-            if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-                filesMap = getAllFiles(dirPath + "/" + file, filesMap)
-            } else {
-                filesMap.set(file, {filename: path.join(dirPath, "/", file)}); //[path, matchedWithMeta]
-            }
-        })
+    files.forEach(function (file) {
+      if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+        filesMap = this._getAllFiles(dirPath + "/" + file, filesMap);
+      } else {
+        filesMap.set(file, { filename: path.join(dirPath, "/", file) }); //[path, matchedWithMeta]
+      }
+    });
 
-        return filesMap;
-    }
+    return filesMap;
+  }
 
-    getAllKeys() {
-        return Array.from(this._filesMap.keys());
-    }
+  getAllKeys() {
+    return Array.from(this._filesMap.keys());
+  }
 
-    getAllValues() {
-        return Array.from(this._filesMap.values());
-    }
+  getAllValues() {
+    return Array.from(this._filesMap.values());
+  }
 
-    get(key) {
-        return this._filesMap.get(key);
-    }
+  get(key) {
+    return this._filesMap.get(key);
+  }
 
-    set(key, value) {
-        this._filesMap.set(key, value);
-    }
+  set(key, value) {
+    this._filesMap.set(key, value);
+  }
 
-    has(key) {
-        return this._filesMap.has(key);
-    }
+  has(key) {
+    return this._filesMap.has(key);
+  }
 
-    delete(key) {
-        this._filesMap.delete(key);
-    }
+  delete(key) {
+    this._filesMap.delete(key);
+  }
 
-    size() {
-        return this._filesMap.size;
-    }
+  size() {
+    return this._filesMap.size;
+  }
 
-    getDirectory() {
-        return this._directory;
-    }
-
+  getDirectory() {
+    return this._directory;
+  }
 }
 
-module.exports = Rawdata
+module.exports = Rawdata;
